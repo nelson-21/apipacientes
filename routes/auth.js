3c96866b700d46ca346a337a4917b3e5c18619c2
nelson-21 <113944284+nelson-21@users.noreply.github.com>
2025-11-03 11:43:10 -0600

    Delete routes directory

--- routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');  
-const { login, register } = require('../controllers/authControllers');
- 
-const router = express.Router(); 
- 
-router.post('/login', login);
-router.post('/register', register); 
- 
-module.exports = router; 
