ebee2e8d1f3853056f9834d2d2b3d648d59878a8
nelson-21 <113944284+nelson-21@users.noreply.github.com>
2025-11-03 11:19:35 -0600

    Delete models directory

--- models/Paciente.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const pacienteSchema = new mongoose.Schema({
-    nombre: {
-        type: String, required: true
-    }
-});
-
-module.exports = mongoose.model('Paciente', pacienteSchema);
